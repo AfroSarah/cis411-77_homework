44428466420b293a7fe517c701746239c15e9688
AfroSarah <afrosarah@gmail.com>
2014-03-19 10:40:35 -0400

    Adding Pet class into models

---
+++ homework6/PetStore/PetStore/Models/Pet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace PetStore.Models
+{
+    public class Pet
+    {
+        public int petID { get; set; }
+        [Display(Name = "Animal")]
+        public string petName { get; set; }
+        [Display(Name = "Description")]
+        public string petDescription { get; set; }
+        [Display(Name = "Date Received")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime petReceivedDate { get; set; }
+        [Display(Name = "Quantity")]
+        public int petQuantity { get; set; }
+        [Display(Name = "Price")]
+        public decimal petPrice { get; set; }
+        [Display(Name = "Picture URL")]
+        public string petPicture { get; set; }
+
+    }
+
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+
+    }
+}
